438c78aca03751c2eddae1ed9fbd67829e68d0c9
Denning <gichabadenning254@gmail.com>
2024-07-10 21:18:59 +0300

    used array iteration to add and remove items in a list

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+// Example usage:
+destructivelyAppendCat("Ralph");
+console.log(cats);
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+
+
+// Usage
+destructivelyPrependCat('Felix');
+console.log(cats);
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+// Usage
+destructivelyRemoveLastCat();
+console.log(cats);
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+// Usage
+destructivelyRemoveFirstCat();
+console.log(cats);
+
+function appendCat(name){
+    return [...cats, name]
+}
+
+
+function prependCat(name){
+    return[name, ...cats];
+}
+
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+
+function  removeLastCat(){
+    return cats.slice(0,2);
+    
+}
+
